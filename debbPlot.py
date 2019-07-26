66e8e86ea03b73a62a7a78185250919104e4d088
Victoria Fletes <vfletes@cusd.me>
2019-07-26 11:07:19 -0700

    now saves the graph to a png file

--- debbPlot.py
+++ debbPlot.py
@@ -3,37 +3,58 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import os
-
+import math
+#ctrl-r redoes an action
 from IPython import embed as shell
+import datetime as dt
+plt.rcParams.update({'xtick.labelsize': 9})
+def setXY(fileName, stormIndex):
+        x = []
+        y = []
+        xLabels = []
+        RI_name_data = open(fileName[:-4] +'_name' + fileName[-4:],'r') #name file
+        for n in range(stormIndex):
+                head_line = RI_name_data.readline()
+        splitted = head_line.split() #splitting the first line of RITC names list
+        marker = str(splitted[0] + ' ' + splitted[1] + ' ' + splitted[2]) #getting the date and time of the RI of the TC
+        storm = splitted[0] #getting the name of the TC
+        date = splitted[1]#beginning storm date
+        stormYr = date[:2]#year of storm
+        count = 0
+        with open(fileName,'r') as RI_data:
+                row = RI_data.readline()
+                while row:
+                        line = row.split()
+                        sYU = line[1]
+                        stormYearU = str(sYU[:2])
+                        if line[-1] == 'HEAD' and line[0] == storm and stormYr == stormYearU:
+                                xVar = str(line[1]) + str(line[2])
+                                xLabels.append(xVar)
+                                date = dt.datetime.strptime(xVar,'%y%m%d%H')
+                                xVar = float(xVar)
+                                #xVal = math.floor(uxVar/100.)*100. + ((uxVar/100.)%1.0)*25./.06 #uxVar was the date+hour-yearFromBeginning
+                                xVal = int(date.strftime('%s'))
+                                x.append(xVal)
+                                line2 = RI_data.readline().split()
+                                y.append(int(line2[2]))
 
-x = []
-y = []
-
-RI_data = open('/calval_npp2/fletes/dataFiles/AL_SHIPS_1982_2017_TC_RI.txt','r')
-RI_name_data = open('/calval_npp2/fletes/dataFiles/AL_SHIPS_1982_2017_TC_RI_name.txt','r')
-head_line = RI_name_data.readline() #getting first line of RITC names list, only need to change this line for storm change!! <-- very important
-head_line = RI_name_data.readline()
-splitted = head_line.split() #splitting the first line of RITC names list
-marker = str(splitted[0] + ' ' + splitted[1] + ' ' + splitted[2]) #getting the date and time of the RI of the TC
-storm = splitted[0] #getting the name of the TC
-date = splitted[1]#beginning storm date
-stormYr = date[:2]#year of storm
-counter = 0
-
-for row in RI_data:
-        line = row.split()
-        if line[-1] == 'HEAD' and line[0] == storm and stormYr in line[1]:
-                x.append(counter)
-                y.append(int(line[3]))
-                if marker in row:
-                        x1 = counter
-                        y1 = int(line[3]) + 5
-                counter += 6
-
-plt.plot(x,y)
-plt.xlabel('Hours Since Storm Data Began')
-plt.ylabel('Wind Speeds')
-plt.title(storm + ' Storm Data')
-plt.errorbar(x1, y1, yerr=5, label='Rapid Intensification Starts') #yerr = line height, xerr = line width
-plt.legend()
+                                if marker in row:
+                                        b = count
+                                        #x1 = xVal
+                                        #y1 = int(line2[2]) + 5
+                                count += 1
+                        row = RI_data.readline()
+        plt.plot(x,y)
+        plt.xticks(x, xLabels, rotation='vertical')
+        plt.xlabel('Year, Month, Day, and Time of Storm')
+        plt.ylabel('Wind Speeds')
+        plt.title(storm + ' Storm Data')
+        #plt.errorbar(x1, 0, yerr=2, label='Rapid Intensification Starts') #yerr = line height, xerr = line width
+        plt.gca().get_xticklabels()[b].set_color('red')
+        #plt.legend()
+w = 10
+h = 6
+fig = plt.figure(figsize=(w,h))
+setXY('/calval_npp2/fletes/dataFiles/AL_SHIPS_1982_2017_TC_RI.txt', 1)
+plt.savefig('/calval_npp2/fletes/dataFiles/alicPlot.png', bbox_inches='tight')
 plt.show()
